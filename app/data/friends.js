1ef6af4094a55f9846c4849603e47b8a7e9f7d55
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-09-14 19:40:40 -0400

    added routes

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+    {
+        "name": "John Smith",
+        "photo": "https://www.biography.com/.image/t_share/MTIwNjA4NjMzOTc0MTk1NzI0/john-smith-9486928-1-402.jpg",
+        "scores": [5,1,3,4,2,4,1,1,3,5]
+    }
+];
+
+module.exports = friends;
